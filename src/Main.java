0f040c2bc03194cbb7ad6b8047eeff25d6bfb546
bianchengxiaobei <443850741@qq.com>
2016-09-08 14:30:20 +0800

    添加了sdl的dll，但是运行还是有错，主要原因我用的jdk是64位的，但是这些dll是32位的

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
-
+import sdljava.SDLException;
+import sdljava.SDLMain;
+import sdljava.video.*;
 public class Main
 {
-
+	static int Screen_Width = 640;
+	static int Screen_Height = 480;
+	static int Screen_Bpp = 32;
+	
+	static SDLSurface message = null;
+	static SDLSurface background = null;
+	static SDLSurface screen = null;
 	public static void main(String[] args) 
 	{
-		
-	}
+		SDLMain sdl = new SDLMain();	
+		try {
+			sdl.init(sdl.SDL_INIT_EVERYTHING);
+			screen = SDLVideo.setVideoMode(Screen_Width, Screen_Height, Screen_Bpp, SDLVideo.SDL_SWSURFACE);
+			if (screen == null)
+			{
 
+				return;
+			}
+			SDLVideo.wmSetCaption("WaterDrawer", null);
+			message = SDLVideo.loadBMP("hello.bmp");
+			background = SDLVideo.loadBMP("background.bmp");
+			
+			sdl.quit();
+		} catch (SDLException e) {
+			// TODO Auto-generated catch block
+			System.err.println("SDL ≥ı ºªØ ß∞‹!");
+			e.printStackTrace();
+		}	
+	}
 }
